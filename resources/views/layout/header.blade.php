d4a0935e30804839f531d4e9385e016ccd7b4c8c
Farhan Noval Hidayat <farhannovaal31@gmail.com>
2024-01-04 06:50:06 +0700

    Push CD

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('font/css/all.min.css') }}">  
+    <title>GreatNews - Viral News - Good News - Hot News</title>
+</head>
+<body>
+    <header class="pxp-header fixed-top bg-red-600 h-16">
+        <div class="container-header flex justify-around pt-5">
+             <div class="title-nav">GreatNews</div>
+             <div class="navbar-menu">
+                <ul class="flex gap-11">
+                    <li>Daily News</li>
+                    <li>Lifestyle</li>
+                    <li>Knowledge</li>
+                    <li>Random Content</li>
+                </ul>
+             </div>
+             <div class="info-menu">
+                <ul class="flex gap-6">
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><i class="fa-solid fa-envelope"></i></li>
+                    | <li><i class="fa-solid fa-magnifying-glass"></i></li>
+                </ul>
+             </div>
+        </div>
+    </header>
+
+
+</body>
+</html>
